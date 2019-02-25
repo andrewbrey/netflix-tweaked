044b550b3b9bda489a419e9a0f8e538dec463df7
Andrew Brey <andrew@fluencyy.com>
2019-02-25 15:22:09 -0800

    Run prettier and bump version in preparation for next release.

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,64 +1,63 @@
-(function (chrome) {
-  'use strict';
+(function(chrome) {
+	"use strict";
 
-  const NT_TABS = {};
-  const NT_ON_BROWSE_SCREEN = 'NT_ON_BROWSE_SCREEN';
-  const NT_OFF_BROWSE_SCREEN = 'NT_OFF_BROWSE_SCREEN';
+	const NT_TABS = {};
+	const NT_ON_BROWSE_SCREEN = "NT_ON_BROWSE_SCREEN";
+	const NT_OFF_BROWSE_SCREEN = "NT_OFF_BROWSE_SCREEN";
 
-  chrome.runtime.onInstalled.addListener(() => {
-    chrome.tabs.query({url: '*://*.netflix.com/*'}, tabs => {
-      tabs.forEach(tab => {
-        NT_TABS[`_${tab.id}`] = {tabId: tab.id, url: tab.url};
-        chrome.tabs.reload(tab.id, {bypassCache: true});
-      });
-    });
-  });
+	chrome.runtime.onInstalled.addListener(() => {
+		chrome.tabs.query({ url: "*://*.netflix.com/*" }, tabs => {
+			tabs.forEach(tab => {
+				NT_TABS[`_${tab.id}`] = { tabId: tab.id, url: tab.url };
+				chrome.tabs.reload(tab.id, { bypassCache: true });
+			});
+		});
+	});
 
-  chrome.runtime.onUpdateAvailable.addListener(() => {
-    console.log('Update listener invoked - reloading runtime.');
-    chrome.runtime.reload();
-  });
+	chrome.runtime.onUpdateAvailable.addListener(() => {
+		console.log("Update listener invoked - reloading runtime.");
+		chrome.runtime.reload();
+	});
 
-  chrome.tabs.onCreated.addListener(tab => {
-    if (tab && tab.url && tab.url.includes('netflix.com')) {
-      NT_TABS[`_${tab.id}`] = {tabId: tab.id, url: tab.url};
-    }
-  });
+	chrome.tabs.onCreated.addListener(tab => {
+		if (tab && tab.url && tab.url.includes("netflix.com")) {
+			NT_TABS[`_${tab.id}`] = { tabId: tab.id, url: tab.url };
+		}
+	});
 
-  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    let ntTab = NT_TABS[`_${tabId}`];
+	chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+		let ntTab = NT_TABS[`_${tabId}`];
 
-    if (ntTab) {
-      ntTab.url = tab.url;
-    } else if (tab && tab.url && tab.url.includes('netflix.com')) {
-      NT_TABS[`_${tab.id}`] = {tabId: tab.id, url: tab.url};
-    }
+		if (ntTab) {
+			ntTab.url = tab.url;
+		} else if (tab && tab.url && tab.url.includes("netflix.com")) {
+			NT_TABS[`_${tab.id}`] = { tabId: tab.id, url: tab.url };
+		}
 
-    determineAndRunTabActions(tabId);
-  });
+		determineAndRunTabActions(tabId);
+	});
 
-  chrome.tabs.onActivated.addListener(activeInfo => {
-    determineAndRunTabActions(activeInfo.tabId);
-  });
+	chrome.tabs.onActivated.addListener(activeInfo => {
+		determineAndRunTabActions(activeInfo.tabId);
+	});
 
-  chrome.tabs.onRemoved.addListener(tabId => {
-    delete NT_TABS[`_${tabId}`];
-  });
+	chrome.tabs.onRemoved.addListener(tabId => {
+		delete NT_TABS[`_${tabId}`];
+	});
 
-  function determineAndRunTabActions(tabId) {
-    let ntTab = NT_TABS[`_${tabId}`];
+	function determineAndRunTabActions(tabId) {
+		let ntTab = NT_TABS[`_${tabId}`];
 
-    if (ntTab && ntTab.url && ntTab.url.match(/netflix\.com\/browse\/?$|netflix\.com\/browse\?.*/)) {
-      chrome.pageAction.show(tabId);
-      chrome.pageAction.setTitle({tabId: tabId, title: 'Netflix Tweaked is Active!'});
+		if (ntTab && ntTab.url && ntTab.url.match(/netflix\.com\/browse\/?$|netflix\.com\/browse\?.*/)) {
+			chrome.pageAction.show(tabId);
+			chrome.pageAction.setTitle({ tabId: tabId, title: "Netflix Tweaked is Active!" });
 
-      chrome.tabs.sendMessage(tabId, NT_ON_BROWSE_SCREEN);
-    } else {
-      chrome.pageAction.hide(tabId);
-      chrome.pageAction.setTitle({tabId: tabId, title: 'Netflix Tweaked is Inactive'});
+			chrome.tabs.sendMessage(tabId, NT_ON_BROWSE_SCREEN);
+		} else {
+			chrome.pageAction.hide(tabId);
+			chrome.pageAction.setTitle({ tabId: tabId, title: "Netflix Tweaked is Inactive" });
 
-      chrome.tabs.sendMessage(tabId, NT_OFF_BROWSE_SCREEN);
-    }
-  }
-
+			chrome.tabs.sendMessage(tabId, NT_OFF_BROWSE_SCREEN);
+		}
+	}
 })(chrome);
--- app/scripts.babel/netflix-tweak.js
+++ app/scripts.babel/netflix-tweak.js
@@ -1,245 +1,247 @@
-(function (chrome) {
-  'use strict';
+(function(chrome) {
+	"use strict";
 
-  document.getElementsByTagName('html')[0].classList.add('netflix-tweaked-loaded');
-  waitForAndRunCadmiumTweak();
+	document.getElementsByTagName("html")[0].classList.add("netflix-tweaked-loaded");
+	waitForAndRunCadmiumTweak();
 
-  const NT_ON_BROWSE_SCREEN = 'NT_ON_BROWSE_SCREEN';
-  const NT_OFF_BROWSE_SCREEN = 'NT_OFF_BROWSE_SCREEN';
+	const NT_ON_BROWSE_SCREEN = "NT_ON_BROWSE_SCREEN";
+	const NT_OFF_BROWSE_SCREEN = "NT_OFF_BROWSE_SCREEN";
 
-  let browseScreenVideos = null;
-  let browseScreenVideoInterval = null;
+	let browseScreenVideos = null;
+	let browseScreenVideoInterval = null;
 
-  let listsParentElement = null;
-  let listsMutationObserver = null;
-  let runawayMutationCounter = 0;
-  let listMoveInterval = null;
+	let listsParentElement = null;
+	let listsMutationObserver = null;
+	let runawayMutationCounter = 0;
+	let listMoveInterval = null;
 
-  let listReorderMessageShown = false;
+	let listReorderMessageShown = false;
 
-  chrome.runtime.onMessage.addListener(message => {
-    switch (message) {
-      case NT_ON_BROWSE_SCREEN: {
-        onBrowseScreen();
-        break;
-      }
-      case NT_OFF_BROWSE_SCREEN: {
-        offBrowseScreen();
-        break;
-      }
-      default: {
-        ntLog(`Unknown message from Netflix Tweaked backend: ${JSON.stringify(message)}`);
-        break;
-      }
-    }
-    return true;
-  });
+	chrome.runtime.onMessage.addListener(message => {
+		switch (message) {
+			case NT_ON_BROWSE_SCREEN: {
+				onBrowseScreen();
+				break;
+			}
+			case NT_OFF_BROWSE_SCREEN: {
+				offBrowseScreen();
+				break;
+			}
+			default: {
+				ntLog(`Unknown message from Netflix Tweaked backend: ${JSON.stringify(message)}`);
+				break;
+			}
+		}
+		return true;
+	});
 
-  function onBrowseScreen() {
-    if(!listReorderMessageShown) {
-      listReorderMessageShown = true;
+	function onBrowseScreen() {
+		if (!listReorderMessageShown) {
+			listReorderMessageShown = true;
 
-      showListReorderOverlay();
-      setTimeout(hideListReorderOverlay, 1500);
-    }
+			showListReorderOverlay();
+			setTimeout(hideListReorderOverlay, 1500);
+		}
 
-    if(browseScreenVideos === null) {
-      browseScreenVideos = document.getElementsByTagName('video');
-    }
+		if (browseScreenVideos === null) {
+			browseScreenVideos = document.getElementsByTagName("video");
+		}
 
-    if(browseScreenVideoInterval === null) {
+		if (browseScreenVideoInterval === null) {
+			browseScreenVideoInterval = setInterval(function() {
+				if (browseScreenVideos && browseScreenVideos.length) {
+					clearInterval(browseScreenVideoInterval);
+					browseScreenVideoInterval = null;
 
-      browseScreenVideoInterval = setInterval(function(){
-        if(browseScreenVideos && browseScreenVideos.length) {
-          clearInterval(browseScreenVideoInterval);
-          browseScreenVideoInterval = null;
+					[].forEach.call(browseScreenVideos, browseScreenVideo => {
+						shutItUp(browseScreenVideo);
 
-          [].forEach.call(browseScreenVideos, browseScreenVideo => {
-            shutItUp(browseScreenVideo);
+						["play", "playing"].forEach(eventName => {
+							browseScreenVideo.addEventListener(eventName, function() {
+								shutItUp(browseScreenVideo);
+							});
+						});
 
-            ['play', 'playing'].forEach(eventName => {
-              browseScreenVideo.addEventListener(eventName, function () {
-                shutItUp(browseScreenVideo);
-              });
-            });
+						function shutItUp(video) {
+							ntLog(
+								"Go away auto-play trailer! Re-making auto-play trailer inert. It's an epic battle with the Netflix code to keep it this way ¯\\_(ツ)_/¯"
+							);
+							video.muted = true;
+							video.playbackRate = 0;
+							video.setAttribute("preload", 0);
+							video.setAttribute("autoplay", "false");
+							video.pause();
+							video.play = function() {};
+						}
+					});
+				}
+			}, 500);
+		}
 
-            function shutItUp(video){
-              ntLog("Go away auto-play trailer! Re-making auto-play trailer inert. It's an epic battle with the Netflix code to keep it this way ¯\\_(ツ)_/¯");
-              video.muted = true;
-              video.playbackRate = 0;
-              video.setAttribute('preload', 0);
-              video.setAttribute('autoplay', 'false');
-              video.pause();
-              video.play = function () {};
-            }
-          });
-        }
-      }, 500);
+		if (listsMutationObserver === null) {
+			if (listMoveInterval === null) {
+				listMoveInterval = setInterval(function() {
+					let billboard = document.querySelector(".billboard-row");
+					let queue = document.querySelector("[data-list-context=queue]");
+					let continueWatching = document.querySelector("[data-list-context=continueWatching]");
 
-    }
+					if ((queue || continueWatching) && document.querySelector(".lolomo")) {
+						clearInterval(listMoveInterval);
+						listMoveInterval = null;
 
-    if(listsMutationObserver === null) {
-      if(listMoveInterval === null) {
-        listMoveInterval = setInterval(function () {
+						listsParentElement = document.querySelector(".lolomo");
 
-          let billboard = document.querySelector('.billboard-row');
-          let queue = document.querySelector('[data-list-context=queue]');
-          let continueWatching = document.querySelector('[data-list-context=continueWatching]');
+						setTimeout(function() {
+							if (billboard) {
+								if (queue && continueWatching) {
+									billboard.insertAdjacentElement("afterend", queue);
+									billboard.insertAdjacentElement("afterend", continueWatching);
+								} else if (queue) {
+									billboard.insertAdjacentElement("afterend", queue);
+								} else {
+									billboard.insertAdjacentElement("afterend", continueWatching);
+								}
+							} else {
+								if (queue && continueWatching) {
+									listsParentElement.insertAdjacentElement("afterbegin", queue);
+									listsParentElement.insertAdjacentElement("afterbegin", continueWatching);
+								} else if (queue) {
+									listsParentElement.insertAdjacentElement("afterbegin", queue);
+								} else {
+									listsParentElement.insertAdjacentElement("afterbegin", continueWatching);
+								}
+							}
 
-          if ((queue || continueWatching) && document.querySelector('.lolomo')) {
-            clearInterval(listMoveInterval);
-            listMoveInterval = null;
+							makeListsOpaque();
+							watchLists();
+						}, 500);
+					}
+				}, 50);
+			}
+		}
 
-            listsParentElement = document.querySelector('.lolomo');
+		function watchLists() {
+			if (listsParentElement !== null) {
+				listsMutationObserver = new MutationObserver(reorderFollowingMutation);
+				listsMutationObserver.observe(listsParentElement, { childList: true });
+			}
+		}
 
-            setTimeout(function(){
-              if(billboard) {
-                if(queue && continueWatching) {
-                  billboard.insertAdjacentElement('afterend', queue);
-                  billboard.insertAdjacentElement('afterend', continueWatching);
-                } else if(queue) {
-                  billboard.insertAdjacentElement('afterend', queue);
-                } else {
-                  billboard.insertAdjacentElement('afterend', continueWatching);
-                }
-              } else {
-                if(queue && continueWatching) {
-                  listsParentElement.insertAdjacentElement('afterbegin', queue);
-                  listsParentElement.insertAdjacentElement('afterbegin', continueWatching);
-                } else if(queue) {
-                  listsParentElement.insertAdjacentElement('afterbegin', queue);
-                } else {
-                  listsParentElement.insertAdjacentElement('afterbegin', continueWatching);
-                }
-              }
+		function reorderFollowingMutation() {
+			ntLog("Netflix code reordered the lists - Ensuring correct order is still present.");
 
-              makeListsOpaque();
-              watchLists();
-            }, 500);
+			runawayMutationCounter++;
+			let allRows = document.querySelectorAll(".lolomoRow");
+			let billboard = document.querySelector(".billboard-row");
+			let queue = document.querySelector("[data-list-context=queue]");
+			let continueWatching = document.querySelector("[data-list-context=continueWatching]");
 
-          }
-        }, 50);
-      }
+			if (allRows && allRows.length && (queue || continueWatching) && runawayMutationCounter < 5000) {
+				if (billboard) {
+					if (queue && continueWatching) {
+						if (
+							!(allRows[0].dataset.listContext === "continueWatching" && allRows[1].dataset.listContext === "queue")
+						) {
+							billboard.insertAdjacentElement("afterend", queue);
+							billboard.insertAdjacentElement("afterend", continueWatching);
+						}
+					} else if (queue) {
+						if (allRows[0].dataset.listContext !== "queue") {
+							billboard.insertAdjacentElement("afterend", queue);
+						}
+					} else {
+						if (allRows[0].dataset.listContext !== "continueWatching") {
+							billboard.insertAdjacentElement("afterend", continueWatching);
+						}
+					}
+				} else {
+					if (queue && continueWatching) {
+						if (
+							!(allRows[0].dataset.listContext === "continueWatching" && allRows[1].dataset.listContext === "queue")
+						) {
+							listsParentElement.insertAdjacentElement("afterbegin", queue);
+							listsParentElement.insertAdjacentElement("afterbegin", continueWatching);
+						}
+					} else if (queue) {
+						if (allRows[0].dataset.listContext !== "queue") {
+							listsParentElement.insertAdjacentElement("afterbegin", queue);
+						}
+					} else {
+						if (allRows[0].dataset.listContext !== "continueWatching") {
+							listsParentElement.insertAdjacentElement("afterbegin", continueWatching);
+						}
+					}
+				}
+			}
+		}
+	}
 
-    }
+	function offBrowseScreen() {
+		makeListsOpaque();
 
-    function watchLists() {
-      if(listsParentElement !== null) {
-        listsMutationObserver = new MutationObserver(reorderFollowingMutation);
-        listsMutationObserver.observe(listsParentElement, { childList: true } );
-      }
-    }
+		if (listsMutationObserver !== null) {
+			listsMutationObserver.disconnect();
+		}
 
-    function reorderFollowingMutation() {
-      ntLog('Netflix code reordered the lists - Ensuring correct order is still present.');
+		if (browseScreenVideoInterval !== null) {
+			clearInterval(browseScreenVideoInterval);
+		}
 
-      runawayMutationCounter++;
-      let allRows = document.querySelectorAll('.lolomoRow');
-      let billboard = document.querySelector('.billboard-row');
-      let queue = document.querySelector('[data-list-context=queue]');
-      let continueWatching = document.querySelector('[data-list-context=continueWatching]');
+		if (listMoveInterval !== null) {
+			clearInterval(listMoveInterval);
+		}
 
-      if(allRows && allRows.length && (queue || continueWatching) && runawayMutationCounter < 5000) {
-        if(billboard) {
-          if(queue && continueWatching) {
-            if(!(allRows[0].dataset.listContext === 'continueWatching' && allRows[1].dataset.listContext === 'queue')) {
-              billboard.insertAdjacentElement('afterend', queue);
-              billboard.insertAdjacentElement('afterend', continueWatching);
-            }
-          } else if(queue) {
-            if(allRows[0].dataset.listContext !== 'queue') {
-              billboard.insertAdjacentElement('afterend', queue);
-            }
-          } else {
-            if(allRows[0].dataset.listContext !== 'continueWatching') {
-              billboard.insertAdjacentElement('afterend', continueWatching);
-            }
-          }
-        } else {
-          if(queue && continueWatching) {
-            if(!(allRows[0].dataset.listContext === 'continueWatching' && allRows[1].dataset.listContext === 'queue')) {
-              listsParentElement.insertAdjacentElement('afterbegin', queue);
-              listsParentElement.insertAdjacentElement('afterbegin', continueWatching);
-            }
-          } else if(queue) {
-            if(allRows[0].dataset.listContext !== 'queue') {
-              listsParentElement.insertAdjacentElement('afterbegin', queue);
-            }
-          } else {
-            if(allRows[0].dataset.listContext !== 'continueWatching') {
-              listsParentElement.insertAdjacentElement('afterbegin', continueWatching);
-            }
-          }
-        }
-      }
-    }
-  }
+		browseScreenVideos = null;
+		browseScreenVideoInterval = null;
 
-  function offBrowseScreen() {
-    makeListsOpaque();
+		listsParentElement = null;
+		listsMutationObserver = null;
+		listMoveInterval = null;
 
-    if(listsMutationObserver !== null) {
-      listsMutationObserver.disconnect();
-    }
+		runawayMutationCounter = 0;
+	}
 
-    if(browseScreenVideoInterval !== null) {
-      clearInterval(browseScreenVideoInterval);
-    }
+	function makeListsOpaque() {
+		if (!document.querySelector("style.nt-make-opaque")) {
+			let css = ".lolomo > :not(.billboard-row) { opacity: 1 !important; }";
+			let head = document.head || document.getElementsByTagName("head")[0];
+			let style = document.createElement("style");
 
-    if(listMoveInterval !== null) {
-      clearInterval(listMoveInterval);
-    }
+			style.type = "text/css";
+			style.classList.add("nt-make-opaque");
+			style.appendChild(document.createTextNode(css));
 
-    browseScreenVideos = null;
-    browseScreenVideoInterval = null;
+			head.appendChild(style);
+		}
+	}
 
-    listsParentElement = null;
-    listsMutationObserver = null;
-    listMoveInterval = null;
+	function hideListReorderOverlay() {
+		if (document.querySelector(".lolomo.reorder-in-progress")) {
+			document.querySelector(".lolomo.reorder-in-progress").classList.remove("reorder-in-progress");
+		}
+	}
 
-    runawayMutationCounter = 0;
-  }
+	function showListReorderOverlay() {
+		if (document.querySelector(".lolomo:not(.reorder-in-progress)")) {
+			document.querySelector(".lolomo:not(.reorder-in-progress)").classList.add("reorder-in-progress");
+		}
+	}
 
-  function makeListsOpaque() {
-    if (!document.querySelector('style.nt-make-opaque')) {
+	function waitForAndRunCadmiumTweak() {
+		ntLog("Tweaking the Netflix cadmium player to prevent trailers");
 
-      let css = '.lolomo > :not(.billboard-row) { opacity: 1 !important; }';
-      let head = document.head || document.getElementsByTagName('head')[0];
-      let style = document.createElement('style');
+		let script = document.createElement("script");
+		script.src = chrome.extension.getURL("scripts/cadmium-player-tweak.js");
+		script.onload = function() {
+			this.remove();
+		};
+		(document.head || document.documentElement).appendChild(script);
+	}
 
-      style.type = 'text/css';
-      style.classList.add('nt-make-opaque');
-      style.appendChild(document.createTextNode(css));
-
-      head.appendChild(style);
-    }
-  }
-
-  function hideListReorderOverlay() {
-    if(document.querySelector('.lolomo.reorder-in-progress')) {
-      document.querySelector('.lolomo.reorder-in-progress').classList.remove('reorder-in-progress');
-    }
-  }
-
-  function showListReorderOverlay() {
-    if(document.querySelector('.lolomo:not(.reorder-in-progress)')) {
-      document.querySelector('.lolomo:not(.reorder-in-progress)').classList.add('reorder-in-progress');
-    }
-  }
-
-  function waitForAndRunCadmiumTweak() {
-    ntLog('Tweaking the Netflix cadmium player to prevent trailers');
-
-    let script = document.createElement('script');
-    script.src = chrome.extension.getURL('scripts/cadmium-player-tweak.js');
-    script.onload = function () {
-      this.remove();
-    };
-    (document.head || document.documentElement).appendChild(script);
-  }
-
-  function ntLog(what) {
-    console.log(`%c  Netflix Tweaked -> ${typeof what === 'object' ? JSON.stringify(what) : what.toString()}  `, 'font-size:14px;color:white;background-color:#2E2E2E;');
-  }
-
+	function ntLog(what) {
+		console.log(
+			`%c  Netflix Tweaked -> ${typeof what === "object" ? JSON.stringify(what) : what.toString()}  `,
+			"font-size:14px;color:white;background-color:#2E2E2E;"
+		);
+	}
 })(chrome);
